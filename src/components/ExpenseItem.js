8d3f30d4da7d9508d815e8dcc18664df019a1d45
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-06-16 18:14:35 +0530

    added ExpenseItem.css file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
-function ExpenseItem(){
-    return(
-        <div>
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return (
+        <div className ="expense-item">
             <div>Date</div>
-            <div>Title</div>
-            <div>Amount</div>
+            <div className = "expense-item__description">
+                <h2>Title</h2>
+                <div className='expense-item__price'>Amount</div>
+            </div>
         </div>
     );
 }
