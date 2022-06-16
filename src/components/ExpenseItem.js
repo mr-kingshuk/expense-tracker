5153f28b570fac12686acb7f000a9059b812f1ff
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-06-16 18:31:06 +0530

    passed data via "props"

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021,2,28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = '230.05';
+//need not necessary to name it props(also called attributes)
+//in props we get the key value pair of the attributes of teh TAG
+function ExpenseItem(props) {
+    // Outputting Dyanmic Data
+    // const expenseDate = new Date(2021,2,28);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = '230.05';
 
     return (
         <div className ="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.expenseDate.toISOString()}</div>
             <div className = "expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.expenseTitle}</h2>
+                <div className='expense-item__price'>${props.expenseAmount}</div>
             </div>
         </div>
     );
