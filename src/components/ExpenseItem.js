801f432c74d27bd33e314938d36d8a7ec4678fa0
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-06-16 18:36:32 +0530

    added Js code in ExpenseItem.js outside JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,14 +3,17 @@ import './ExpenseItem.css';
 //need not necessary to name it props(also called attributes)
 //in props we get the key value pair of the attributes of teh TAG
 function ExpenseItem(props) {
-    // Outputting Dyanmic Data
-    // const expenseDate = new Date(2021,2,28);
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = '230.05';
+    const month = props.expenseDate.toLocaleString('en-US', {month: 'long'});
+    const day = props.expenseDate.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
 
     return (
         <div className ="expense-item">
-            <div>{props.expenseDate.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className = "expense-item__description">
                 <h2>{props.expenseTitle}</h2>
                 <div className='expense-item__price'>${props.expenseAmount}</div>
