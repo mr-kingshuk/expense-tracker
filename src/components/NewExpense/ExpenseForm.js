4ca78e232183f0f7cdb6614d7aeb6ff39ef61145
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-06-17 15:53:17 +0530

    adding only 1 state variable

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, { useSate, useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+
+    //using state here to detach the variable from the lifecycel of teh components, so that even if the components are reevaluated the variable still exixts.
+
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+
+    //since all 3 states are related to our form, so we might take 1 state only.
+    //catch is we need to update all 3 of them
+
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    })
+
+    const titleChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value
+        });
+    };
+
+    const amountChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value
+        });
+    };
+
+    const dateChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        });
+    };
+
+
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" />
+                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
 
