4a977edf5b094d45f145eb96b2ad21e54870baad
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-06-17 15:35:55 +0530

    Added NewExpense.js and ExpenseForm.js compoenents

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -7,22 +7,24 @@ import './ExpenseItem.css';
 //need not necessary to name it props(also called attributes)
 //in props we get the key value pair of the attributes of teh TAG
 const ExpenseItem = (props) => {
-
-    const clickHandler = ()=>{
-        comsole.log('clicked');
-    };
+    //Directly inside component
+    //returns 2 values and receives an INITIAL value
+    const [title, setTitle] = useState(props.expenseTitle);
     
+    const clickHandler = ()=>{
+        // instead of directly changing the title variable we call teh state changing function
+        setTitle('Updated');
+    };
+
     //forwarding data from props to children components or using the data
     return (
         //now here we are getting the styles of Card component
-
-        //but if we want to use custom components as wrappers, we need to pass a props to Card.js and pass all the children components to it.(done in Card.js)
 
         //Now className is also passed as attribute instaed of acting as ClassName
         <Card className ="expense-item">
             <ExpenseDate date = {props.expenseDate}/>
             <div className = "expense-item__description">
-                <h2>{props.expenseTitle}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.expenseAmount}</div>
             </div>
             
