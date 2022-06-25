46a27f6634b1c0bd57b60b21c99f423d1325de2a
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-06-25 10:48:23 +0530

    Rendering Lists of Dta and Adding Keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
@@ -9,35 +9,26 @@ const Expenses = (props) => {
 
     const [filteredYear, setFilteredYear] = useState('2020');
 
-    const filterChangeHandler = (year) =>{
+    const filterChangeHandler = (year) => {
         setFilteredYear(year);
     }
 
     return (
-            <Card className="expenses">
-                <ExpensesFilter
-                selected = {filteredYear}
-                onChangeFilter = {filterChangeHandler} />
-                <ExpenseItem
-                    expenseDate={props.expenses[0].date}
-                    expenseTitle={props.expenses[0].title}
-                    expenseAmount={props.expenses[0].amount} />
+        <Card className="expenses">
+            <ExpensesFilter
+                selected={filteredYear}
+                onChangeFilter={filterChangeHandler} />
 
-                <ExpenseItem
-                    expenseDate={props.expenses[1].date}
-                    expenseTitle={props.expenses[1].title}
-                    expenseAmount={props.expenses[1].amount} />
+            {/* Mapping the expense array to an array of ExpenseItem Component */}
 
-                <ExpenseItem
-                    expenseDate={props.expenses[2].date}
-                    expenseTitle={props.expenses[2].title}
-                    expenseAmount={props.expenses[2].amount} />
+            {props.expenses.map(expense => <ExpenseItem 
+            key = {expense.id} 
+            expenseDate={expense.date}
+                expenseTitle={expense.title}
+                expenseAmount={expense.amount} />)};
 
-                <ExpenseItem
-                    expenseDate={props.expenses[3].date}
-                    expenseTitle={props.expenses[3].title}
-                    expenseAmount={props.expenses[3].amount} />
-            </Card>
+
+        </Card>
     );
 }
 
